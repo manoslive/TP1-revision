36f74627dcd95693b3a51552d415b5bc95ba2903
manoslive <ebeloin@hotmail.com>
2015-01-31 11:04:42 -0500

    Initial commit

---
+++ Stage.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace TP1___Refresh
+{
+    partial class Stage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LB_Entreprises = new System.Windows.Forms.ListBox();
+            this.TB_NumStage = new System.Windows.Forms.TextBox();
+            this.TB_TypeStage = new System.Windows.Forms.TextBox();
+            this.BTN_Precedent = new System.Windows.Forms.Button();
+            this.BTN_Suivant = new System.Windows.Forms.Button();
+            this.LB_Numstage = new System.Windows.Forms.Label();
+            this.LB_Description = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RTB_Description = new System.Windows.Forms.RichTextBox();
+            this.BTN_Ajouter = new System.Windows.Forms.Button();
+            this.BTN_Sauvegarder = new System.Windows.Forms.Button();
+            this.BTN_Effacer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LB_Entreprises
+            // 
+            this.LB_Entreprises.FormattingEnabled = true;
+            this.LB_Entreprises.ItemHeight = 20;
+            this.LB_Entreprises.Location = new System.Drawing.Point(554, 21);
+            this.LB_Entreprises.Name = "LB_Entreprises";
+            this.LB_Entreprises.Size = new System.Drawing.Size(183, 224);
+            this.LB_Entreprises.TabIndex = 0;
+            this.LB_Entreprises.SelectedIndexChanged += new System.EventHandler(this.LB_Entreprises_SelectedIndexChanged);
+            // 
+            // TB_NumStage
+            // 
+            this.TB_NumStage.Enabled = false;
+            this.TB_NumStage.Location = new System.Drawing.Point(175, 21);
+            this.TB_NumStage.Name = "TB_NumStage";
+            this.TB_NumStage.Size = new System.Drawing.Size(175, 26);
+            this.TB_NumStage.TabIndex = 1;
+            // 
+            // TB_TypeStage
+            // 
+            this.TB_TypeStage.Location = new System.Drawing.Point(175, 173);
+            this.TB_TypeStage.Name = "TB_TypeStage";
+            this.TB_TypeStage.Size = new System.Drawing.Size(175, 26);
+            this.TB_TypeStage.TabIndex = 1;
+            // 
+            // BTN_Precedent
+            // 
+            this.BTN_Precedent.Location = new System.Drawing.Point(57, 221);
+            this.BTN_Precedent.Name = "BTN_Precedent";
+            this.BTN_Precedent.Size = new System.Drawing.Size(112, 34);
+            this.BTN_Precedent.TabIndex = 2;
+            this.BTN_Precedent.Text = "Précédent";
+            this.BTN_Precedent.UseVisualStyleBackColor = true;
+            this.BTN_Precedent.Click += new System.EventHandler(this.BTN_Precedent_Click);
+            // 
+            // BTN_Suivant
+            // 
+            this.BTN_Suivant.Location = new System.Drawing.Point(238, 221);
+            this.BTN_Suivant.Name = "BTN_Suivant";
+            this.BTN_Suivant.Size = new System.Drawing.Size(112, 34);
+            this.BTN_Suivant.TabIndex = 2;
+            this.BTN_Suivant.Text = "Suivant";
+            this.BTN_Suivant.UseVisualStyleBackColor = true;
+            this.BTN_Suivant.Click += new System.EventHandler(this.BTN_Suivant_Click);
+            // 
+            // LB_Numstage
+            // 
+            this.LB_Numstage.AutoSize = true;
+            this.LB_Numstage.Location = new System.Drawing.Point(38, 21);
+            this.LB_Numstage.Name = "LB_Numstage";
+            this.LB_Numstage.Size = new System.Drawing.Size(131, 20);
+            this.LB_Numstage.TabIndex = 3;
+            this.LB_Numstage.Text = "Numéro de stage";
+            // 
+            // LB_Description
+            // 
+            this.LB_Description.AutoSize = true;
+            this.LB_Description.Location = new System.Drawing.Point(38, 70);
+            this.LB_Description.Name = "LB_Description";
+            this.LB_Description.Size = new System.Drawing.Size(89, 20);
+            this.LB_Description.TabIndex = 3;
+            this.LB_Description.Text = "Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Type de stage";
+            // 
+            // RTB_Description
+            // 
+            this.RTB_Description.Location = new System.Drawing.Point(175, 67);
+            this.RTB_Description.Name = "RTB_Description";
+            this.RTB_Description.Size = new System.Drawing.Size(175, 82);
+            this.RTB_Description.TabIndex = 4;
+            this.RTB_Description.Text = "";
+            // 
+            // BTN_Ajouter
+            // 
+            this.BTN_Ajouter.Location = new System.Drawing.Point(385, 40);
+            this.BTN_Ajouter.Name = "BTN_Ajouter";
+            this.BTN_Ajouter.Size = new System.Drawing.Size(112, 34);
+            this.BTN_Ajouter.TabIndex = 2;
+            this.BTN_Ajouter.Text = "Ajouter stage";
+            this.BTN_Ajouter.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Sauvegarder
+            // 
+            this.BTN_Sauvegarder.Location = new System.Drawing.Point(385, 93);
+            this.BTN_Sauvegarder.Name = "BTN_Sauvegarder";
+            this.BTN_Sauvegarder.Size = new System.Drawing.Size(112, 34);
+            this.BTN_Sauvegarder.TabIndex = 2;
+            this.BTN_Sauvegarder.Text = "Sauvegarder";
+            this.BTN_Sauvegarder.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Effacer
+            // 
+            this.BTN_Effacer.Location = new System.Drawing.Point(385, 147);
+            this.BTN_Effacer.Name = "BTN_Effacer";
+            this.BTN_Effacer.Size = new System.Drawing.Size(112, 34);
+            this.BTN_Effacer.TabIndex = 2;
+            this.BTN_Effacer.Text = "Effacer";
+            this.BTN_Effacer.UseVisualStyleBackColor = true;
+            // 
+            // Stage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 278);
+            this.Controls.Add(this.RTB_Description);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LB_Description);
+            this.Controls.Add(this.LB_Numstage);
+            this.Controls.Add(this.BTN_Effacer);
+            this.Controls.Add(this.BTN_Sauvegarder);
+            this.Controls.Add(this.BTN_Ajouter);
+            this.Controls.Add(this.BTN_Suivant);
+            this.Controls.Add(this.BTN_Precedent);
+            this.Controls.Add(this.TB_TypeStage);
+            this.Controls.Add(this.TB_NumStage);
+            this.Controls.Add(this.LB_Entreprises);
+            this.Name = "Stage";
+            this.Text = "Stage";
+            this.Load += new System.EventHandler(this.Stage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LB_Entreprises;
+        private System.Windows.Forms.TextBox TB_NumStage;
+        private System.Windows.Forms.TextBox TB_TypeStage;
+        private System.Windows.Forms.Button BTN_Precedent;
+        private System.Windows.Forms.Button BTN_Suivant;
+        private System.Windows.Forms.Label LB_Numstage;
+        private System.Windows.Forms.Label LB_Description;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox RTB_Description;
+        private System.Windows.Forms.Button BTN_Ajouter;
+        private System.Windows.Forms.Button BTN_Sauvegarder;
+        private System.Windows.Forms.Button BTN_Effacer;
+    }
+}
---
+++ Stage.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace TP1___Refresh
+{
+    public partial class Stage : Form
+    {
+        DataSet stageDS = new DataSet();
+        OracleConnection oracon = new OracleConnection();
+        private String nomEntreprise { get; set; }
+
+        private string Dsource = "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)" +
+"(HOST=205.237.244.251)(PORT=1521)))" + "(CONNECT_DATA=(SERVICE_NAME=ORCL.clg.qc.ca)))";
+        public Stage()
+        {
+            InitializeComponent();
+        }
+        public bool Connect()
+        {
+            bool resultat = false;
+            try
+            {
+                string chaineConnection = "Data Source = " + Dsource + ";User Id =cooperch;Password=ORACLE1";
+                oracon.ConnectionString = chaineConnection;
+                oracon.Open();
+                if (oracon.State == ConnectionState.Open)
+                {
+                    if (MessageBox.Show("Vous êtes maintenant connecté!" + "\n" + "Bienvenue dans un monde rempli d'intrigues!", "État de connection à la BD") == DialogResult.OK)
+                    {
+                        resultat = true;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("ERREUUUUR", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    resultat = false;
+                }
+
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            return resultat;
+        }
+        private void Stage_Load(object sender, EventArgs e)
+        {
+            Connect();
+            RemplirFormulaire();
+        }
+        private void RemplirFormulaire()
+        {
+            OracleCommand oraSelect = oracon.CreateCommand();
+            oraSelect.CommandText = "Select * From StagesEntreprises " +
+                                    "where NomEnt=:NomEnt";
+            OracleParameter OraParamNomEnt = new OracleParameter(":NomEnt", OracleDbType.Varchar2);
+            OraParamNomEnt.Value = 102;
+            oraSelect.Parameters.Add(OraParamNomEnt);
+            using (OracleDataAdapter oraAdapter = new OracleDataAdapter(oraSelect))
+            {
+                if (stageDS.Tables.Contains("STAGES"))
+                {
+                    stageDS.Tables["STAGES"].Clear();
+                }
+                oraAdapter.Fill(stageDS, "STAGES");
+                oraAdapter.Dispose();
+            }
+            UpdateLinkTB();
+        }
+        private void UpdateLinkTB()
+        {
+            UnbindTB();
+            TB_NumStage.DataBindings.Add("Text", stageDS, "STAGES.NumStg");
+            RTB_Description.DataBindings.Add("Text", stageDS, "STAGES.Description");
+            TB_TypeStage.DataBindings.Add("Text", stageDS, "STAGES.TypeStg");
+        }
+        private void UnbindTB()
+        {
+            TB_NumStage.DataBindings.Clear();
+            TB_NumStage.Clear();
+            RTB_Description.DataBindings.Clear();
+            RTB_Description.Clear();
+            TB_TypeStage.DataBindings.Clear();
+            TB_TypeStage.Clear();
+        }
+        /*  private void Ajouter()
+          {
+              string sql = "insert into FicheJoueur" +
+                           "(NumeroMatch, NumeroJoueur, NombreButs, NombrePasses, TempsPunition) " +
+                           "Values(:NumeroMatch,:NumeroJoueur,:NombreButs,:NombrePasses,:TempsPunition)";
+              try
+              {
+                  OracleCommand oraAjout = new OracleCommand(sql, oracon);
+
+                  OracleParameter OraParaNumeroMatch = new OracleParameter(":NumeroMatch", OracleDbType.Int32);
+                  OracleParameter OraParamNumeroJoueur = new OracleParameter(":NumeroJoueur", OracleDbType.Varchar2, 40);
+
+
+                  OraParaNumeroMatch.Value = aJ.numMatch;
+
+
+                  oraAjout.Parameters.Add(OraParaNumeroMatch);
+
+                  oraAjout.ExecuteNonQuery();
+                  RemplirFormulaire();
+              }
+              catch (OracleException ex) // Erreur "child exists"
+              {
+                  if (ex.Number == 2292)
+                      MessageBox.Show("Le joueur ne doit pas avoir de statistique dans les matchs", "Erreur 2292", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                  else
+                      MessageBox.Show(ex.Message.ToString());
+              }
+          }
+          private void Modifier()
+          {
+              Form_Ajouter_Stats ajs = new Form_Ajouter_Stats(oracon, connection);
+              ajs.isModif = false;
+              ajs.callBackForm = this;
+              ajs.Text = "Modification des Statistiques";
+              ajs.numMatch = TB_NumMatch.Text;
+              ajs.numJoueur = TB_NumJoueur.Text;
+              ajs.nbButs = TB_NbButs.Text;
+              ajs.nbPasses = TB_NbPasses.Text;
+              ajs.tempsPunition = TB_TempsPunition.Text;
+              ajs.Location = this.Location;
+              this.Hide();
+
+              if (ajs.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+              {
+                  string sql = "update FicheJoueur set NumeroMatch=:NumeroMatch, NumeroJoueur=:NumeroJoueur, NombreButs=:NombreButs, NombrePasses=:NombrePasses, TempsPunition=:TempsPunition " +
+                                  "where numerojoueur=:numerojoueur";
+                  try
+                  {
+
+                      OracleCommand oraAjout = new OracleCommand(sql, oracon);
+
+                      OracleParameter OraParaNumeroMatch = new OracleParameter(":NumeroMatch", OracleDbType.Int32);
+                      OracleParameter OraParamNumeroJoueur = new OracleParameter(":NumeroJoueur", OracleDbType.Int32);
+                      OracleParameter OraParamNombreButs = new OracleParameter(":NombreButs", OracleDbType.Int32);
+                      OracleParameter OraParaNombrePasses = new OracleParameter(":NombrePasses", OracleDbType.Int32);
+                      OracleParameter OraParaTempsPunition = new OracleParameter(":TempsPunition", OracleDbType.Int32);
+
+                      OraParaNumeroMatch.Value = ajs.numMatch;
+                      OraParamNumeroJoueur.Value = ajs.numJoueur;
+                      OraParamNombreButs.Value = ajs.nbButs;
+                      OraParaNombrePasses.Value = ajs.nbPasses;
+                      OraParaTempsPunition.Value = ajs.tempsPunition;
+
+                      oraAjout.Parameters.Add(OraParaNumeroMatch);
+                      oraAjout.Parameters.Add(OraParamNumeroJoueur);
+                      oraAjout.Parameters.Add(OraParamNombreButs);
+                      oraAjout.Parameters.Add(OraParaNombrePasses);
+                      oraAjout.Parameters.Add(OraParaTempsPunition);
+
+                      oraAjout.ExecuteNonQuery();
+
+                      RemplirFormulaire();
+                  }
+                  catch (OracleException ex)
+                  {
+                      MessageBox.Show(ex.Message.ToString());
+                  }
+
+              }
+          }
+          private void Supprimer()
+          {
+              if (MessageBox.Show("Voulez-vous vraiment effacer cette entrée ?", "Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+              {
+                  try
+                  {
+                      OracleParameter paramNomEquipe = new OracleParameter(":numjoueur", OracleDbType.Int32);
+                      paramNomEquipe.Value = TB_NumJoueur.Text;
+                      string sql = "Delete from joueur Where numerojoueur =:numjoueur";
+                      OracleCommand oraDelete = new OracleCommand(sql, oracon);
+                      oraDelete.Parameters.Add(paramNomEquipe);
+                      oraDelete.ExecuteNonQuery();
+                      RemplirFormulaire();
+                  }
+                  catch (OracleException ex)
+                  {
+                      if (ex.Number == 2292) // Erreur "child exists"
+                          MessageBox.Show("Vous ne pouvez pas effacer des joueurs avec des matchs", "Erreur 2292", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                      else
+                          MessageBox.Show(ex.Message.ToString());
+                  }
+              }
+          }
+         */
+
+        private void BTN_Precedent_Click(object sender, EventArgs e)
+        {
+            this.BindingContext[stageDS, "STAGES"].Position--;
+        }
+
+        private void BTN_Suivant_Click(object sender, EventArgs e)
+        {
+            this.BindingContext[stageDS, "STAGES"].Position++;
+        }
+
+        private void LB_Entreprises_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            nomEntreprise = LB_Entreprises.SelectedItem.ToString();
+        }
+    }
+}
